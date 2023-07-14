a16545687f40ebd417e7c7b7dbb01632990ddb47
Justine <justinestephanie4@gmail.com>
2023-07-14 03:57:47 -0700

    I sometimes suffer from insomnia.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -8,6 +10,9 @@
 int main(void)
 {
 	char letter;
+
+	srand(time(0));
+	n = rand - RAND_MAX / 2;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
